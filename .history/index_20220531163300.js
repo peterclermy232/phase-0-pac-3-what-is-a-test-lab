50e3aa81b05af33da86dab3e669498768b07d097
peterclermy <peterclermy232@gmail.com>
2022-05-31 16:36:34 +0300

    I have solve the problem and test pass

--- .history/index_20220531163300.js
+++ .history/index_20220531163300.js
@@ -1,5 +1,5 @@
 const name = "Susan";
-const height = 37;
+const height = 40;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
--- .history/index_20220531163331.js
+++ .history/index_20220531163331.js
@@ -1,5 +1,5 @@
 const name = "Susan";
-const height = 37;
+const height = ;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
